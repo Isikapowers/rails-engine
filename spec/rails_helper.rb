a07d939611d49dcc2ee8d56a2ec7a850e310a2a0
iPowers <isika.powers@gmail.com>
2021-10-23 10:23:22 -0600

    Add require factory_bot.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,7 @@
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'support/factory_bot'
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
@@ -65,7 +66,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  
+
 end
 
 Shoulda::Matchers.configure do |config|
