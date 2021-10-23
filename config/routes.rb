5a6e2a45acad97c9a7d4c4c22298847eeba051d3
iPowers <isika.powers@gmail.com>
2021-10-23 14:14:11 -0600

    Add routes.

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: %i[index show]
+      get '/merchants/find', to: 'merchants#find'
+      get '/merchants/find_all', to: 'merchants#find_all'
+      resources :merchants, only: [:index, :show]
       get '/merchants/:id/items', to: 'merchant_items#index'
+
+      get '/items/find', to: 'items#find'
+      get '/items/find_all', to: 'items#find_all'
       resources :items
       get '/items/:id/merchant', to: 'item_merchants#show'
     end
