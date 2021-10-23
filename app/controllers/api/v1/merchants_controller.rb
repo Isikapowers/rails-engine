e7b450f33ca77dd404ec403fa0c1eae449e99e57
iPowers <isika.powers@gmail.com>
2021-10-22 22:24:45 -0600

    Create merchants controller.

---
+++ app/controllers/api/v1/merchants_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::MerchantsController < ApplicationController
+  def index
+    get_page
+    get_per_page
+    merchants = Merchant.get_list(get_page, get_per_page)
+    render json: MerchantSerializer.new(merchants)
+  end
+
+  def show
+    merchant = Merchant.find(params[:id])
+    render json: MerchantSerializer.new(merchant)
+  end
+end
