df5d5ec5471d15c437d52f55924991bb837208c1
iPowers <isika.powers@gmail.com>
2021-10-26 13:41:48 -0600

    Create serializers.

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,7 +1,8 @@
-# frozen_string_literal: true
-
 class ItemSerializer
   include JSONAPI::Serializer
 
-  attributes :name, :description, :unit_price, :merchant_id
+  attributes :name,
+             :description,
+             :unit_price,
+             :merchant_id
 end
